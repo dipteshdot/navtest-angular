5ed30f86bf6d6067375429a1d7bef8a36fdbc99b
dipteshdot <59639218+dipteshdot@users.noreply.github.com>
2021-01-05 12:07:46 +0530

    Add files via upload

---
+++ src/app/material.module.ts
@@ -0,0 +1,31 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+// import { 
+// //   MatButtonModule, 
+//   MatIconModule, 
+//   MatMenuModule,
+//   MatToolbarModule,
+// } from '@angular/material';
+import { MatButtonModule } from '@angular/material/button'
+import { MatIconModule } from '@angular/material/icon'
+import { MatMenuModule } from '@angular/material/menu'
+import { MatToolbarModule } from '@angular/material/toolbar'
+
+
+
+@NgModule({
+  imports: [ 
+    MatButtonModule, 
+    MatIconModule, 
+    MatMenuModule,
+    MatToolbarModule,
+  ],
+  exports: [
+    MatButtonModule, 
+    MatIconModule, 
+    MatMenuModule,
+    MatToolbarModule,
+  ]    
+})
+
+export class MaterialModule {}
